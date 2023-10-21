5375b4421a0849f6fe1b6c424c4e530a9945fffe
Tereza Smolova <tereza.smolova@gmail.com>
2023-10-21 14:41:36 +0200

    Repair in Layer

--- src/components/Layer/index.jsx
+++ src/components/Layer/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 export const Layer = ({ color, layer })=>{
     return(
         <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-            <div className="layer__label">{name}</div>
+            <div className="layer__color" style={color}></div>
+            <div className="layer__label">{layer}</div>
         </div>
 )}
